8faa9ef67afbd41f80d5b76cc90b6e3efa88d283
wandl <hnxxyhcwdl1003@163.com>
2022-05-29 23:02:31 +0800

    yyy

--- src/main/java/org/apache/shiro/spring/boot/kisso/authz/KissoPermissionAnnotationHandler.java
+++ src/main/java/org/apache/shiro/spring/boot/kisso/authz/KissoPermissionAnnotationHandler.java
@@ -20,7 +20,6 @@ import java.lang.annotation.Annotation;
 import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.authz.aop.AuthorizingAnnotationHandler;
 
-import com.baomidou.kisso.annotation.Action;
 import com.baomidou.kisso.annotation.Permission;
 
 /**
@@ -28,16 +27,16 @@ import com.baomidou.kisso.annotation.Permission;
  * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
  */
 public class KissoPermissionAnnotationHandler extends AuthorizingAnnotationHandler {
-    
+
 	public KissoPermissionAnnotationHandler() {
 		super(Permission.class);
 	}
-	
+
 	@Override
 	public void assertAuthorized(Annotation a) throws AuthorizationException {
 		Permission pm = (Permission) a;
 		if (pm != null) {
-            if (pm.action() == Action.Skip) {
+            if (pm.ignore()) {
                 // 忽略检查
             } else {
                 // 权限检查
