acb4c97d08e9821a981cd933bc3f3380816d4b9c
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:12:13 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/shiro/spring/boot/kisso/authz/KissoPermissionAnnotationMethodInterceptor.java
+++ src/main/java/org/apache/shiro/spring/boot/kisso/authz/KissoPermissionAnnotationMethodInterceptor.java
@@ -20,7 +20,7 @@ import org.apache.shiro.authz.aop.AuthorizingAnnotationMethodInterceptor;
 
 /**
  * TODO
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 
 public class KissoPermissionAnnotationMethodInterceptor extends AuthorizingAnnotationMethodInterceptor {
