17c0853adf42fb9ffa5b5561f233a008842859c6
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 17:12:07 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/shiro/spring/boot/kisso/KissoStatelessPrincipalRepository.java
+++ src/main/java/org/apache/shiro/spring/boot/kisso/KissoStatelessPrincipalRepository.java
@@ -34,7 +34,7 @@ import com.google.common.collect.Sets;
 
 /**
  * Kisso Token Principal Repository
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class KissoStatelessPrincipalRepository extends ShiroPrincipalRepositoryImpl{
 	
--- src/main/java/org/apache/shiro/spring/boot/kisso/authz/KissoAuthorizationFilter.java
+++ src/main/java/org/apache/shiro/spring/boot/kisso/authz/KissoAuthorizationFilter.java
@@ -30,7 +30,7 @@ import com.baomidou.kisso.security.token.SSOToken;
 
 /**
  * Kisso 授权 (authorization) 过滤器
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class KissoAuthorizationFilter extends AbstracAuthorizationFilter {
 
